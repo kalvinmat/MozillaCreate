ca34d39b315c2ce956a5e50e07c1553d3725ac90
Bryannttt <Bgarci157@calstatela.edu>
2022-11-23 19:34:04 -0800

    Changed App.js

--- mozzi/src/App.js
+++ mozzi/src/App.js
@@ -16,38 +16,40 @@ function App() {
   const [savedNotes, setSavedNotes] = useState([])
 
   useEffect(() => {
-    handleListen()
+    const handleListen = () => {
+      if (isListening) {
+        mic.start()
+        mic.onend = () => {
+          console.log('continue..')
+          mic.start()
+        }
+      } else {
+        mic.stop()
+        mic.onend = () => {
+          console.log('Stopped Mic on Click')
+        }
+      }
+      mic.onstart = () => {
+        console.log('Mics on')
+      }
+
+      mic.onresult = event => {
+        const transcript = Array.from(event.results)
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join('')
+        console.log(transcript)
+        setNote(transcript)
+        mic.onerror = event => {
+          console.log(event.error)
+        }
+      }
+    };
+
+    handleListen();
+
   }, [isListening])
 
-  const handleListen = () => {
-    if (isListening) {
-      mic.start()
-      mic.onend = () => {
-        console.log('continue..')
-        mic.start()
-      }
-    } else {
-      mic.stop()
-      mic.onend = () => {
-        console.log('Stopped Mic on Click')
-      }
-    }
-    mic.onstart = () => {
-      console.log('Mics on')
-    }
-
-    mic.onresult = event => {
-      const transcript = Array.from(event.results)
-        .map(result => result[0])
-        .map(result => result.transcript)
-        .join('')
-      console.log(transcript)
-      setNote(transcript)
-      mic.onerror = event => {
-        console.log(event.error)
-      }
-    }
-  }
 
   const handleSaveNote = () => {
     setSavedNotes([...savedNotes, note])
