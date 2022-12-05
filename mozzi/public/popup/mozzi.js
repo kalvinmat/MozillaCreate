0b8c54fa666c4cbaff067c3ff388772607b30aab
SimonMai6 <simonmai2009@gmail.com>
2022-12-05 12:03:07 -0800

    Added the ability to google search
    Now opens a new tab and opens to a google search engine and what you searched for. Changed from a window popping up and going to the google search engine.

--- mozzi/public/popup/mozzi.js
+++ mozzi/public/popup/mozzi.js
@@ -1,10 +1,14 @@
-document.addEventListener("click", (e) => {
-        let creating = browser.windows.create({
-          type: "popup",
-          url: ["http://www.google.com"]
-        });
-        creating.then(() => {
-          console.log("The popup has been created");
-        });
-  });
+const f = document.getElementById('form');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=';
+
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + '+' + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
   
